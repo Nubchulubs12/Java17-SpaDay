340ba4dec5348495354cc6b53b4f57ec1eb0e4a6
Nubchulubs12 <nick.kuczka@gmail.com>
2024-10-17 20:29:08 -0500

    done

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,18 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters long.")
     private String username;
+    @Email(message = "Invalid email")
     private String email;
+    @Size(min = 6, message = "passwored must be at least 6 characters long.")
     private String password;
+    @NotNull(message = "passwords do not match")
+    private String verifypassword;
 
     public User() {
 
@@ -14,6 +23,16 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifypassword = verifypassword;
+    }
+
+    public @NotNull String getVerifypassword() {
+        return verifypassword;
+    }
+
+    public void setVerifypassword(String verifypassword) {
+        this.verifypassword = verifypassword;
+        checkPassword();
     }
 
     public String getUsername() {
@@ -37,7 +56,17 @@ public class User {
     }
 
     public void setPassword(String password) {
-        this.password = password;
-    }
-}
 
+        this.password = password;
+
+    }
+
+    public void checkPassword() {
+        if(password !=null && verifypassword != null && !password.equals(verifypassword)) {
+            verifypassword = null;
+            }
+
+        }
+
+
+}
